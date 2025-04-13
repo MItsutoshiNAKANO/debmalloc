2e319cf167202d58f1cb8461bba50b3408ad2621
Mitsutoshi Nakano <ItSANgo@gmail.com>
2025-04-13 14:17:47 +0900

    Add a makefile for a test module

---
+++ test_debmalloc.mk
@@ -0,0 +1,33 @@
+#! /usr/bin/make -f
+
+##
+# @file
+# @brief The Makefile for the test code for the debmalloc.
+# @copyright
+#   2025 Mitsutoshi Nakano <ItSANgo@gmail.com>
+#   SPDX-License-Identifier: Apache-2.0
+
+TARGETS=test_debmalloc
+OBJS=test_debmalloc.o
+SOURCES=$(OBJS:.o=.c)
+DEPS=$(SOURCES:.c=.d)
+
+CFLAGS=-Wall -Werror -Wextra -g
+LDLIBS=-L. -ldebmalloc
+
+ifdef USE_DEBMALLOC
+CFLAGS+=-DUSE_DEBMALLOC=$(USE_DEBMALLOC)
+LDLIBS+=-L. -ldebmalloc
+endif # USE_DEBMALLOC
+
+.PHONY: all clean
+
+all: $(TARGETS)
+test_debmalloc: $(OBJS)
+%.d: %.c
+	$(CC) $(CFLAGS) -MM -o $@ $<
+format:
+	clang-format -i $(SOURCES) $(HEADERS)
+clean:
+	$(RM) $(TARGETS) $(OBJS) $(DEPS)
+-include $(DEPS)
