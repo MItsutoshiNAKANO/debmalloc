863362f815bc419dfa5a0ca0c7a963d608243c35
Mitsutoshi Nakano <ItSANgo@gmail.com>
2025-09-13 07:55:10 +0900

    Fix the macros arguments

--- debmalloc_macros.h
+++ debmalloc_macros.h
@@ -4,27 +4,29 @@
 ///   2025 Mitsutoshi Nakano
 ///   SPDX-License-Identifier: Apache-2.0
 /// @author Mitsutoshi Nakano <ItSANgo@gmail.com>
-/// @version 1.0.0
-/// @date 2025-03-09
+/// @version 1.0.1
+/// @date 2025-09-13
 
 #if defined(USE_DEBMALLOC) && defined(DEBMALLOC_PRIORITY)
 
 #define malloc(size) \
-    debmalloc_malloc(size, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
+    debmalloc_malloc((size), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
 #define free(ptr) \
-    debmalloc_free(ptr, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
+    debmalloc_free((ptr), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
 #define calloc(nmemb, size) \
     debmalloc_calloc( \
-        nmemb, size, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__ \
+        (nmemb), (size), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__ \
     )
 #define realloc(ptr, size) \
     debmalloc_realloc( \
-        ptr, size, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__ \
+        (ptr), (size), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__ \
     )
 #define strdup(s) \
-    debmalloc_strdup(s, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
+    debmalloc_strdup((s), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
 #define strndup(s, n) \
-    debmalloc_strndup(s, n, DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__)
+    debmalloc_strndup( \
+        (s), (n), DEBMALLOC_PRIORITY, __FILE__, __LINE__, __func__ \
+    )
 
 #else // defined(USE_DEBMALLOC) && defined(DEBMALLOC_PRIORITY)
 
